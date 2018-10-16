ff6776c3d551709a4e0fe3e5bd53929b4ea36f26
Kranck <frizzy.rastay@gmail.com>
2018-10-16 16:22:41 +0200

    Added first tests with Selenium for calendar

---
+++ src/squashTA/resources/selenium/java/DashboardCalendar.java
@@ -0,0 +1,72 @@
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.junit.Assert.fail;
+
+public class DashboardCalendar {
+    private HtmlUnitDriver driver;
+    private String baseUrl;
+    private String valX;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new HtmlUnitDriver();
+        baseUrl = "http://m2gl.deptinfo-st.univ-fcomte.fr";
+    }
+
+  @Test
+  public void testDashboardCalendar() throws Exception {
+    driver.get(baseUrl + "m2gl.deptinfo-st.univ-fcomte.fr/~m2test2/preprod");
+    driver.findElement(By.name("username")).clear();
+    driver.findElement(By.name("username")).sendKeys("admin");
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("admin");
+    driver.findElement(By.name("submit")).click();
+    driver.findElement(By.linkText("Calendrier")).click();
+    assertTrue(driver.findElement(By.xpath("//body")).isDisplayed());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
