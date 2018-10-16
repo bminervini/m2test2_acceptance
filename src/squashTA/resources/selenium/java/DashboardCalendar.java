ce626bb88d16a436db99e0f234d10f4047119a0f
Kranck <frizzy.rastay@gmail.com>
2018-10-16 16:46:25 +0200

    I hope, the final fix

--- src/squashTA/resources/selenium/java/DashboardCalendar.java
+++ src/squashTA/resources/selenium/java/DashboardCalendar.java
@@ -18,7 +18,7 @@ public class DashboardCalendar {
 
   @Test
   public void testDashboardCalendar() throws Exception {
-    driver.get(baseUrl + "m2gl.deptinfo-st.univ-fcomte.fr/~m2test2/preprod/static/login.php");
+    driver.get(baseUrl + "http://m2gl.deptinfo-st.univ-fcomte.fr/~m2test2/preprod/static/login.php");
     // Thread.sleep(2000);
     driver.findElement(By.name("username")).clear();
     driver.findElement(By.name("username")).sendKeys("admin");
