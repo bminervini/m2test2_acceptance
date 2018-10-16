5fa33539ef5973a0373c3aca96eada13ba88153d
Bminervini <baptiste.minervini@outlook.fr>
2018-10-16 12:37:32 +0200

    simple access test selenium

--- src/squashTA/resources/selenium/java/AccessTest.java
+++ src/squashTA/resources/selenium/java/AccessTest.java
@@ -1,30 +1,27 @@
 import org.junit.*;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-
 import static org.junit.Assert.fail;
 
-public class ConnectionTest {
+public class AccessTest
+{
     private HtmlUnitDriver driver;
     private String baseUrl;
     private String valX;
     private StringBuffer verificationErrors = new StringBuffer();
-
     @Before
     public void setUp() throws Exception {
         driver = new HtmlUnitDriver();
         baseUrl = "http://m2gl.deptinfo-st.univ-fcomte.fr";
     }
     @Test
-    public void test2() throws Exception {
+    public void testAccess() throws Exception {
         driver.get("http://m2gl.deptinfo-st.univ-fcomte.fr/~m2test2/preprod/static/login.php");
-        driver.findElement(By.name("username")).click();
-        driver.findElement(By.name("username")).clear();
-        driver.findElement(By.name("username")).sendKeys("admin");
-        driver.findElement(By.name("password")).clear();
-        driver.findElement(By.name("password")).sendKeys("admin");
-        driver.findElement(By.name("submit")).click();
     }
+
     @After
     public void tearDown() throws Exception {
         driver.quit();
