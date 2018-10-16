d209aaca3d6d65a6669a486be357dc64cc49faa1
Bminervini <baptiste.minervini@outlook.fr>
2018-10-16 10:26:37 +0200

    first test linked with squash

--- src/squashTA/resources/selenium/java/ConnectionTest.java
+++ src/squashTA/resources/selenium/java/ConnectionTest.java
@@ -1,77 +1,24 @@
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-import java.util.concurrent.TimeUnit;
-
-import static org.junit.Assert.fail;
-
-public class ConnectionTest
-{
-    private WebDriver driver;
+public class ConnectionTest {
+    private HtmlUnitDriver driver;
     private String baseUrl;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
-
+    private String valX;
     @Before
     public void setUp() throws Exception {
-        driver = new FirefoxDriver();
-        baseUrl = "https://www.katalon.com/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver = new HtmlUnitDriver();
+        baseUrl = "http://m2gl.deptinfo-st.univ-fcomte.fr";
     }
-
     @Test
     public void test2() throws Exception {
-        driver.get("http://localhost:3030/static/login.php");
+        driver.get("http://m2gl.deptinfo-st.univ-fcomte.fr/~m2test2/preprod/static/login.php");
         driver.findElement(By.name("username")).click();
         driver.findElement(By.name("username")).clear();
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).clear();
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("submit")).click();
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
-        }
     }
 }
