0c9c7dfbd5828a20d1f50f613b37a4b1f2048625
Bminervini <baptiste.minervini@outlook.fr>
2018-10-22 14:27:56 +0200

    fix on personne test class 3

--- src/squashTA/resources/selenium/java/AddUser.java
+++ src/squashTA/resources/selenium/java/AddUser.java
@@ -1,13 +1,6 @@
-package com.example.tests;
-
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class AddUser {
