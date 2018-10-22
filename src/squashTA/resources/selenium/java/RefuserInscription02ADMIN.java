d2b1fd5f6c24475e69f936977fff3f2176401094
Kranck <frizzy.rastay@gmail.com>
2018-10-22 16:52:52 +0200

    Started 01ADMIN test

---
+++ src/squashTA/resources/selenium/java/RefuserInscription02ADMIN.java
@@ -0,0 +1,48 @@
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.junit.Assert.fail;
+
+public class RefuserInscription02ADMIN {
+    private HtmlUnitDriver driver;
+    private String baseUrl;
+    private String valX;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new HtmlUnitDriver();
+        baseUrl = "http://m2gl.deptinfo-st.univ-fcomte.fr";
+    }
+
+  @Test
+  public void test03CONN() throws Exception {
+    driver.get("http://m2gl.deptinfo-st.univ-fcomte.fr/~m2test2/preprod/static/login.php");
+    driver.findElement(By.name("username")).click();
+    driver.findElement(By.name("username")).clear();
+    driver.findElement(By.name("username")).sendKeys("admin");
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("admin");
+    driver.findElement(By.name("submit")).click();
+    driver.findElement(By.linkText("Utilisateurs")).click();
+    driver.findElement(By.name("add")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Cron'])[1]/following::i[1]")).click();
+    driver.findElement(By.name("username")).click();
+    driver.findElement(By.name("username")).clear();
+    driver.findElement(By.name("username")).sendKeys("luc");
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("luc");
+    driver.findElement(By.name("submit")).click();
+    Assert.assertEquals("luc", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Calendrier'])[1]/following::span[1]")).getText());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+}
