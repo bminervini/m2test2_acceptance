3a5cf6e4bdd4ad81451891bc5f9ac6ccb5091f29
Kranck <frizzy.rastay@gmail.com>
2018-10-22 16:24:48 +0200

    Test for 04CONN

--- src/squashTA/resources/selenium/java/AccesInterfaceGestion04ADMIN.java
+++ src/squashTA/resources/selenium/java/AccesInterfaceGestion04ADMIN.java
@@ -28,6 +28,7 @@ public class AccesInterfaceGestion04ADMIN {
         Thread.sleep(2000);
         Assert.assertEquals(driver.getCurrentUrl(), "http://m2gl.deptinfo-st.univ-fcomte.fr/~m2test2/preprod/static/dashboard.php");
         // Assert.assertTrue(driver.findElement(By.xpath("link=Participants")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath(".//link=Participants")).isDisplayed());
     }
     @After
     public void tearDown() throws Exception {
